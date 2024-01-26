8ed518d84235dae1bb8e7060fda0c0c993b15573
Momo L'As <momolas@users.noreply.github.com>
2024-01-26 12:34:38 +0100

    Initial Commit

---
+++ CRAZYQUARTER/CRAZYQUARTERApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRAZYQUARTERApp.swift
+//  CRAZYQUARTER
+//
+//  Created by Mo on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CRAZYQUARTERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
